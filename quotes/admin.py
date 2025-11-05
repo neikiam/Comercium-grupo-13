e5138f0fbb6c55895c058508b5bd3e8886bf0450
brian guzman <neikiam500@gmail.com>
2025-11-05 18:58:54 -0300

    Update

--- quotes/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-
-"""Admin configuration for quotes app (none yet)."""
--- quotes/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-
-"""Models for the quotes app (currently empty)."""
